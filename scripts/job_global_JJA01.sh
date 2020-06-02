704d5ad15074265d6e3c11b4e89504fb2fc5c09a
Delphine Lobelle <d.m.a.lobelle@uu.nl>
2020-06-02 16:22:34 +0200

    adding job shell scripts per season

---
+++ scripts/job_global_JJA01.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -t 5-00:00:00
+#SBATCH -p fat             
+#SBATCH -n 4        
+#SBATCH --mail-type=begin       
+#SBATCH --mail-type=end         
+#SBATCH --mail-type=fail        
+#SBATCH --mail-user=d.m.a.lobelle@uu.nl
+
+echo 'Initiating global run JJA 2001...'
+
+srun python global_Kooi+NEMO_3D.py -mon='06' -yr='2001'
+
+
+echo 'Finished computation.'
